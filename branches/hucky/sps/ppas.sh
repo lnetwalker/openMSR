0849a8e1ad85a48baa03be63105c0e728006a751
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2006-04-11 22:31:33 +0000

    deleted

--- branches/hucky/sps/ppas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DoExitAsm ()
-{ echo "An error occurred while assembling $1"; exit 1; }
-DoExitLink ()
-{ echo "An error occurred while linking $1"; exit 1; }
-echo Linking sps
-/usr/bin/ld  -dynamic-linker=/lib/ld-linux.so.2  -s -L. -o sps link.res
-if [ $? != 0 ]; then DoExitLink sps; fi
