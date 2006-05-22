c136f122d5edeb776a6631b8f2d2599f30c97f66
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2006-05-22 20:01:43 +0000

    * rewritten kop programm to a procedure kop * removed mehrfach * remove load AWL * removed definitions from sps.h * changed code to be saved as linked list internally * fixed bug in TE and ZR commands where pointer to next command   was pointing to the k parameter line instead to the cmd one line further * added command to open text browser window with build kop

--- branches/hucky/sps/kop.pas
+++ branches/hucky/sps/kop.pas
@@ -1,14 +1,10 @@
-program kop;
+procedure kop;
 
 
-{ this program generates a ladder logic diagram ( Kontaktplan ( KOP ))	}
+{ this procedure generates a ladder logic diagram ( Kontaktplan ( KOP ))}
 { from an Instruction list (IL) ( Anweisungsliste (AWL)) program		}
 
 { copyright (c) 2006 by Hartmut Eilers <hartmut@eilers.net>				}
-
-
-{$include ./sps.h}
-
 
 var i				: integer;
 	token			: byte;
@@ -16,28 +12,7 @@ var i				: integer;
 	Beschriftung,
 	dummy,
 	KVal			: string;
-
-
-
-function mehrfach (z:word):boolean;
-
-begin
-     mehrfach:=true;
-     if (operation[z+1]=anweisung[1]) or				{ UN(	}
-        (operation[z+1]=anweisung[3]) or				{ UN	}
-        (operation[z+1]=anweisung[5]) or				{ U(	}
-		(operation[z+1]=anweisung[20]) or				{ NOP	}
-		(operation[z+1]=anweisung[11]) or				{ EN	}
-		(operation[z+1]=anweisung[25]) or				{ PE	}
-		(operation[z+1]=anweisung[30]) or				{ EP	}
-		(operation[z+1]=anweisung[31]) or				{ AN(	}
-		(operation[z+1]=anweisung[32]) or				{ AN	}
-		(operation[z+1]=anweisung[33]) or				{ A(	}
-		(operation[z+1]=anweisung[34]) or				{ A		}
-        (operation[z+1]=anweisung[12]) 					{ U		}
-	 then mehrfach:=false
-end;
-
+	KOP_pointer		: doc_pointer;
 
 function add2string(Zeichenkette:string;Zeichen:char;laenge:byte):string;
 { fügt Zeichen an Zeichenkette bis die Länge = laenge ist }
@@ -49,69 +24,13 @@ begin
 	add2string:=Zeichenkette;
 end;
 
-procedure sps_laden;
-
-var	f              		:text;
-	zeile		   		:string[48];
-	i,code  	   		:integer;   { code is currently a dummy, may be used for error detection }
-	name		   		:string;
-
-procedure get_file_name;           { namen des awl-files einlesen   }
 
 begin
-	write (' Filename : ');
-	readln (name);
-	if pos('.',name)=0 then name:=name+'.sps';
-end;                               { **** ENDE GET_FILE_NAME **** }
-
-
-
-begin
-	i:=0;
-	if paramcount=0 then get_file_name  { keine Aufrufparameter }
-	else begin
-		name:=paramstr(1);
-		if pos('.',name)=0 then name:=name+'.sps';
-	end;
-	assign (f,name);
-	{$I-} reset (f); {$I+}
-	if ioresult <> 0 then
-	begin
-		writeln (' SPS-File nicht gefunden');
-		halt(1);
-	end;
-	writeln(' Lade Programm ',name);
-	while not(eof(f)) do
-	begin
-		inc(i);
-		readln (f,zeile);
-		val (copy(zeile,1,3),znr[i],code);
-		operation[i] := copy(zeile,5,3);
-		operand[i] := zeile[9];
-		val (copy(zeile,11,5),par[i],code);
-		comment[i] := copy (zeile,17,22);
-	end;
-	for i := 1 to anweismax do begin
-		anweisung[i]:=anweis[i];
-		if (length(anweis[i]) < 3) then begin
-			repeat
-				anweisung[i]:=concat(anweisung[i],' ');
-		 	until (length(anweisung[i]) = 3);
-		end;
-	end;
-
-	close (F);
-end;                               {**** ENDE SPS_LADEN **** }
-
-
-begin
-	writeln('kop export V ',version,' (c) 2006 by Hartmut Eilers <hartmut@eilers.net>');
-	writeln('for OpenSPS. ');
-	writeln;
-	sps_laden;
+	if not(programm) then exit;
 	i:=1;
 	Zeile:='|-';
 	Beschriftung:='  ';
+	new(kop_pointer);
 	repeat
 		token:=0;
 		repeat
@@ -141,8 +60,8 @@ begin
 						Beschriftung:=add2string(Beschriftung,' ',60);
 						Zeile:=Zeile+'-( )-|';
 						Beschriftung:=Beschriftung+'  '+operand[i]+dummy+' ';
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						if (mehrfach(i)) then begin
 							Zeile:='';
 							Beschriftung:='';
@@ -157,8 +76,8 @@ begin
 			4,13 : begin	{ ON oder O }
 						Zeile:=Zeile+'----+';
 						Beschriftung:=Beschriftung+'    |';
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						if ( token = 4 ) then
 							Zeile:='|--[/]-'
 						else
@@ -167,15 +86,16 @@ begin
 				 end;
 			11,25,30 : begin	{ EN PE EP }
 						Zeile:='|--END--|';
-						writeln(Zeile);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 				 end;
 			24 : begin	{ $ = execute }
 						Zeile:=add2string(Zeile,'-',54);
 						Beschriftung:=add2string(Beschriftung,' ',54);
 						Zeile:=Zeile+'-(execute)-|';
 						Beschriftung:=Beschriftung+comment[i];
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						Zeile:='|-';
 						Beschriftung:='  ';
 				 end;
@@ -185,10 +105,11 @@ begin
 						str(par[i+1],KVal);
 						Zeile:=Zeile+'-(SE)-|';
 						Beschriftung:=Beschriftung+'  '+operand[i]+dummy+' k='+KVal;
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						Zeile:='|-';
 						Beschriftung:='  ';
+						inc(i);
 				 end;
 			10 : begin { ZR }
 						Zeile:=add2string(Zeile,'-',59);
@@ -196,10 +117,11 @@ begin
 						str(par[i+1],KVal);
 						Zeile:=Zeile+'-(ZR)-|';
 						Beschriftung:=Beschriftung+'  '+operand[i]+dummy+' k='+KVal;
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						Zeile:='|-';
 						Beschriftung:='  ';
+						inc(i);
 				 end;
 			16,17 : begin { S oder R }
 						Zeile:=add2string(Zeile,'-',50);
@@ -215,8 +137,8 @@ begin
 						else
 							Zeile:=Zeile+'-(R)-|';
 						Beschriftung:=Beschriftung+'  '+operand[i]+dummy;
-						writeln(Zeile);
-						writeln(Beschriftung);
+						AppendStringToList(Zeile,KOP_pointer);
+						AppendStringToList(Beschriftung,KOP_pointer);
 						if (mehrfach(i)) then begin
 							Zeile:='';
 							Beschriftung:='';
@@ -229,18 +151,19 @@ begin
 						end;
 				 end;
 			20 : begin { NOP }
-					writeln;
+					AppendStringToList('',KOP_pointer);
 					Zeile:='|-';
 					Beschriftung:='  ';
 				 end;
 			else begin
-					writeln;
-					writeln('Unimplemented AWL Command found, KOP is not correct,');
-					writeln('quitting unexpected!');
+					AppendStringToList('',KOP_pointer);
+					AppendStringToList('Unimplemented AWL Command found, KOP is not correct,',KOP_pointer);
+					AppendStringToList('quitting unexpected!',KOP_pointer);
 					token:=11;
 				 end;
 		end;
 		inc(i);
 	until ((token = 11) or (token = 25) or (token = 30));  { EN PE EP }
-	
-end.
+	browsetext(KOP_pointer,1,2,screenx,screeny);
+	//release(KOP_pointer);
+end;
