96373fb18f4984549daf5d198dfcf550b34bb1d2
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2006-05-22 20:49:16 +0000

    moved to  main dir and included in one source

--- branches/hucky/sps/run_sps/ppas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DoExitAsm ()
-{ echo "An error occurred while assembling $1"; exit 1; }
-DoExitLink ()
-{ echo "An error occurred while linking $1"; exit 1; }
-echo Linking run_sps
-/usr/bin/ld  -dynamic-linker=/lib/ld-linux.so.2  -s -L. -o run_sps link.res
-if [ $? != 0 ]; then DoExitLink run_sps; fi
