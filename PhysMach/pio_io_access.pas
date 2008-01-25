f3c4091131cf753c7cb0683f7ccd75d2d5eaf809
Hartmut Eilers <hartmut@eilers.net>
2008-01-25 15:03:51 +0000

    added conditional compiling for ZAURUS platform, if symbol Zaurus is defined no access to the hardware is made

--- PhysMach/pio_io_access.pas
+++ PhysMach/pio_io_access.pas
@@ -1,7 +1,7 @@
 Unit pio_io_access;
 
 { diese Unit stellt Funktionen zum I/O Access auf			} 
-{ die Ports einer 8255 PIO zur Verfügung  				}	
+{ die Ports einer 8255 PIO zur Verfï¿½gung  				}	
 { If you have improvements please contact me at 			}
 { hartmut@eilers.net							}
 { all code is copyright by Hartmut Eilers and released under		}
@@ -16,8 +16,13 @@ function pio_read_ports(io_port:longint):byte;
 function pio_write_ports(io_port:longint;byte_value:byte):byte;
 function pio_hwinit(initdata:string):boolean;
 
+{$define ZAURUS }
 implementation
-uses oldlinux,ports;
+uses linux
+{$ifndef ZAURUS}
+,ports
+{$endif}
+;
 
 const	
       debug             = false;
@@ -27,15 +32,19 @@ function pio_read_ports(io_port:longint):byte;
 var	byte_value : byte;
 
 begin
+	{$ifndef ZAURUS}
 	ReadPort(io_port,byte_value);
-    if (debug) then writeln('PIO_IO r : ',byte_value);
+	if (debug) then writeln('PIO_IO r : ',byte_value);
 	pio_read_ports:=byte_value;
+	{$endif}
 end;
 	
 function pio_write_ports(io_port:longint;byte_value:byte):byte;	
 begin
     if (debug) then  writeln ('PIO_IO w : ',byte_value);
+	{$ifndef ZAURUS}
 	WritePort(io_port,byte_value);
+	{$endif}
 	pio_write_ports:=0;
 end;
 
@@ -48,16 +57,20 @@ var
 
 begin
 	{ allow full port access for the addressrange $300-$3FF }
+	{$ifndef ZAURUS}
 	IOperm($300,$FF,$ff);
+	{$endif}
 	{ the configport and configword are given in one string as }
 	{ parameter; address and data must be delimited by : }
 	{ eg hwinit('$307:$99'); }
-    delim:=pos(':',initdata);
-    val(copy(initdata,1,delim-1),controlPort);
-    val(copy(initdata,delim+1,length(initdata)-delim),configByte);
+	delim:=pos(':',initdata);
+	val(copy(initdata,1,delim-1),controlPort);
+	val(copy(initdata,delim+1,length(initdata)-delim),configByte);
 	if ( debug ) then
 		writeln('Port : ',controlPort,' ',configByte);
-    WritePort(controlPort,configByte);
+	{$ifndef ZAURUS}
+	WritePort(controlPort,configByte);
+	{$endif}
 	pio_hwinit:=true;
 end;
 
