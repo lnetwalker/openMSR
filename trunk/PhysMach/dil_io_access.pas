ed5e3eb92f78419931b72b502d85170ad9aeec47
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2008-01-25 14:49:05 +0000

    Added conditional compiling for Zaurus Platform If Symbol Zaurus is defined no access to the hardware is made

--- trunk/PhysMach/dil_io_access.pas
+++ trunk/PhysMach/dil_io_access.pas
@@ -1,16 +1,18 @@
 Unit dil_io_access;
 
 { diese Unit stellt Funktionen zum I/O Access auf		} 
-{ die Ports des DIL/Net PC zur Verfügung 			}	
+{ die Ports des DIL/Net PC zur Verfï¿½gung 			}	
 { eigentlich war das die Unit zum Zugriff auf den Printer	}
 { port der 1.7.0 Version. Um einen schnellen Test zu machen	}
-{ habe ich die allernötigsten Änderungen zum Zugriff auf die	}
+{ habe ich die allernï¿½tigsten ï¿½nderungen zum Zugriff auf die	}
 { ports eines DIL/NetPC gemacht!!!				}
 { Attention: its just a raw hack - not finished			}
 { If you have improvements please contact me at 		}
 { hartmut@eilers.net						}
 { all code is copyright by Hartmut Eilers and released under	}
 { the GNU GPL see www.gnu.org for license details		}
+
+{ $define ZAURUS}
 
 INTERFACE
 
@@ -19,7 +21,7 @@ function dil_write_ports(io_port:longint;byte_value:byte):byte;
 function dil_hwinit(initdata:string):boolean;
 
 implementation
-uses oldlinux;
+uses linux;
 
 const	
 	CSCIR=$22;	{ chip setup and control index register }
@@ -35,8 +37,10 @@ function dil_read_ports(io_port:longint):byte;
 var	byte_value : byte;
 begin
 	{ read Data from DIP switch }
+	{$ifndef ZAURUS}
 	WritePort(CSCIR,PBDR);
 	ReadPort(CSCDR,byte_value);
+	{$ifdef}
 	if (debug) then writeln ('DIL: r ',io_port,' -> ',byte_value);
 	dil_read_ports:=byte_value;
 end;
@@ -45,9 +49,11 @@ end;
 function dil_write_ports(io_port:longint;byte_value:byte):byte;	
 { in this dirty hack the port parameter is ignored! }
 begin
+	{$ifndef ZAURUS}
 	{ write data to LED }
 	WritePort(CSCIR,PADR);
 	WritePort(CSCDR,byte_value);
+	{$endif}
 	if (debug) then writeln ('DIL: w ',io_port,' -> ',byte_value);
 end;
 
@@ -57,6 +63,7 @@ begin
 	if (debug) then writeln ('DIL: HWinit ',initdata);
 	{ initstring is a dummy !}
 	{ set the permission to access the ports }
+	{$ifndef ZAURUS}
 	IOperm($22,$ff,$ff);
 	{ set port a of dil pc to output }
 	WritePort(CSCIR,PAMR);
@@ -64,6 +71,7 @@ begin
 	{ set port b of dil pc to input }
 	WritePort(CSCIR,PBMR);
 	WritePort(CSCDR,$00);
+	{$endif}
 end;
 
 
