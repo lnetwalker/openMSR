588a929370f86bc6f2799282f51d3db23c919f4c
Hartmut Eilers <hartmut@eilers.net>
2008-02-14 16:10:59 +0000

    + added constant to move axis text to left + changed loop in funktionswerte_berechnen to allow     action on errors + added coments to explain how the axis is drawn + beautified plot + added code to only start graphics if there are no errors

--- fktplot/fktplot.pas
+++ fktplot/fktplot.pas
@@ -2,6 +2,8 @@ program fktplot;
 
 uses qgtk2,FunctionSolver;
 
+{$Id$}
+
 
 const 
 	version = '0.1';
@@ -10,6 +12,7 @@ const
 	debug = false;
 	GetScreenMaxX=800;
 	GetSCreenMaxY=480;
+	XFontCorr=15;		{ verschiebe Beschriftung um n pixxel nach links }
 
 var 
 	formel       : string80;
@@ -53,7 +56,8 @@ begin
 	ymax:=0;
 	ymin:=0;
 	x[1]:=xmin;
-	for i:=1 to anzahl do begin
+	i:=1;
+	repeat			{ until all values are computed or an error occurs }
 		y[i]:=fx(x[i],funk,zahlen);
 		if FSfm<>0 then begin
 			write(#7);
@@ -63,7 +67,8 @@ begin
 		if y[i]>ymax then ymax:=y[i];
 		if y[i]<ymin then ymin:=y[i];
 		x[i+1]:=x[i]+dx;
-	end;
+		inc(i);
+	until (i>anzahl) or (FSfm<>0);
 	if ymax>abs(ymin) then maxy:=2*ymax
 	else maxy:=2* abs(ymin);
 	if debug then writeln('maxy=',maxy);
@@ -99,26 +104,28 @@ procedure onCreate;
 		k:=20;
 		qdrawtext(j,k,formel);
 		(* Y-Achse *)
-		for i:=3 downto -3 do begin
+		for i:=3 downto -3 do begin { jeweils 3 Werte im positiven und negativen Bereich }
 			k:=GetScreenMaxY-Y_rand-trunc((i*maxy/6)*yfact+zero_line)+y_ver;
-			qline(x_rand-4,k,x_rand+4,k);
-			if i<>0 then str(i*maxy/6:6:3,y_wert)
+			qline(x_rand-4,k,x_rand+4,k); { Wertelinie }
+			if i<>0 then str(i*maxy/6:6:3,y_wert) { Y Wert in String wandeln }
 			else Y_wert:=' 0.00';
-			qdrawtext(0,k,y_wert);
+			qdrawtext(0,k,y_wert);		{ Y Wert ausgeben }
 		end;
 		(* X-Achse *)
 		i:=0;
+		{ koordinaten des 1. Wertes ermitteln }
 		j:=x_rand;
-		k:=GetScreenMaxY-y_rand+y_ver+24;
+		k:=GetScreenMaxY-y_rand+y_ver+14;
+		{ umwandeln und ausgeben }
 		str(x[1]:4:2,x_wert);
-		qdrawtext(j,k,x_wert);
+		qdrawtext(j-XFontCorr,k,x_wert);
 		repeat
-			inc(i,40);
+			inc(i,40);	{ um 40 Werte weiter rechts nächste Beschriftung }
 			j:=i*xfact+x_rand;
-			k:=GetScreenMaxY-y_rand+y_ver;
-			qline (j,k+8,j,k+12);
+			k:=GetScreenMaxY-y_rand+y_ver-10;
+			qline (j,k+8,j,k+12); { Beschriftungslinie }
 			str(x[i]:6:3,x_wert);
-			qdrawtext(j,k+24,x_wert);
+			qdrawtext(j-XFontCorr,k+24,x_wert);
 		until i=240;
          end;
 
@@ -126,7 +133,7 @@ procedure onCreate;
 begin
 	x_rand:=60;
 	y_rand:=80;
-	y_ver:=15;
+	y_ver:=55;
 	xfact:=trunc((GetScreenMaxX-80)/anzahl);
 	yfact:=(GetScreenMaxY-y_rand-20)/maxy;
 	y_axis:=GetScreenMaxY-y_rand-10;
@@ -141,10 +148,10 @@ begin
 	zero_line:=trunc((GetScreenMaxY-y_rand)/2);
 
 	{ Y-Achse zeichnen }
-	qline(x_rand,0+y_ver,x_rand,GetScreenMaxY-y_rand);
+	qline(x_rand,0+y_ver,x_rand,GetScreenMaxY-y_rand+y_ver);
 
 	{ X Achse zeichnen }
-	qline(x_rand,y_axis+20+y_ver,xfact*anzahl+x_rand+10,y_axis+20+y_ver);
+	qline(x_rand,y_axis+y_ver+10,xfact*anzahl+x_rand+10,y_axis+y_ver+10);
 	qline(x_rand,zero_line+y_ver,xfact*anzahl+x_rand+10,zero_line+y_ver);
 	beschriftung;
 	funktion_zeichnen;
@@ -159,7 +166,9 @@ begin
 	holparm;
 	codier(formel,funk,zahlen);
 	if FSfm=0 then funktionswerte_berechnen;
-	if FSfm=0 then qdrawstart(800,480, @onCreate,nil, nil);
+	if FSfm=0 then begin
+		qdrawstart(800,480, @onCreate,nil, nil);
 
-	qgo;
+		qgo;
+	end;
 end.
