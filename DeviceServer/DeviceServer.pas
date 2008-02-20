d8f1314d282d7256b86bde36ba4ff6c5892d3709
Hartmut Eilers <hartmut@eilers.net>
2008-02-20 23:15:16 +0000

    + first frame, test libs

---
+++ DeviceServer/DeviceServer.pas
@@ -0,0 +1,14 @@
+program DeviceServer;
+
+uses PhysMach,webserver;
+
+{ $Id$ }
+
+{ This software is copyright (c) 2008 by Hartmut Eilers <hartmut@eilers.net> 	}
+{ It is distributed under the terms of the GNU GPL V2 see http://www.gnu.org 	}
+
+{ 20.02.2008		Start of project					}
+
+begin
+
+end.
