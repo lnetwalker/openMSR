c9061f3254d173fd031f52261599c5e12c8e3a6c
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2008-03-17 00:05:01 +0000

    +removed BinToInt and used unit CommonHelper instead

--- trunk/PhysMach/http_io_access.pas
+++ trunk/PhysMach/http_io_access.pas
@@ -21,7 +21,7 @@ function http_write_ports(io_port:longint;byte_value:byte):byte;
 function http_hwinit(initdata:string):boolean;
 
 implementation
-uses linux
+uses linux,CommonHelper
 {$ifndef ZAURUS}
 ,http
 {$endif}
@@ -32,58 +32,6 @@ const
 
 var
 	R_URL,W_URL		: string;
-	power	: array [1..8] of byte =(1,2,4,8,16,32,64,128);
-
-
-
-function BinToInt(binval:string):Integer;
-var
-	i,k,wert	: Integer;
-begin
-	i:=1;
-	k:=1;
-	wert:=0;
-	while i <= length(binval) do begin				{ wert errechnen }
-
-		if debug then writeln('html_io_access exec_read_ports Loop: char_pointer=',i,' BinCalcPointer=',k,' Value=',wert);
- 
-		case binval[i] of
-			'1' : 	begin			{ 1 speichern }
-					wert:=wert+power[k];
-					inc(k);
-				end;
-			'0' :	inc(k);			{ 0 merken }
-			' ' :	if debug then writeln('blank detected ');	{ blanks ignorieren }
-		else							{ fehlerhafter return wert }
-			if debug then writeln('html_io_access ERROR: wrong return value ',binval);
-		end;
-		inc(i);
-	end;
-	BinToInt:=wert;
-end;
-
-
-{ removes all HTML tags from a document }
-function deHTML(page:AnsiString):AnsiString;
-var
-	htmlfree		: AnsiString;
-	EndTag,StartTag		: LongInt;
-
-begin
-	htmlfree:=page;
-	if debug then writeln('deHTML original page: ',page);
-	{ HTML header wegwerfen }
-	EndTag:=pos('>',htmlfree);
-	htmlfree:=copy(htmlfree,EndTag+1,length(htmlfree)-EndTag);
-	repeat
-		StartTag:=pos('<',htmlfree);
-		EndTag:=pos('>',htmlfree);
-		htmlfree:=copy(htmlfree,1,StartTag-1)+copy(htmlfree,EndTag+1,length(htmlfree));
-	until (pos('<',htmlfree)=0);
-	if debug then writeln('deHTML`ed page: ',htmlfree);
-	deHTML:=htmlfree;
-end;
-
 
 	
 function http_read_ports(io_port:longint):byte;
