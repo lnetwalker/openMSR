58039189a62fc8ac91390ed7cc345fbd014abce6
Hartmut Eilers <hartmut@eilers.net>
2008-03-17 00:12:36 +0000

    + PhysMach must be able to read specific devices instead of all inputs - therefore added PhysMachGetDevices which returns a list of known devices and PhysMachIOByDevice + in order to reuse and share code I introduced new private functions to read and write analog and digital values.

--- PhysMach/PhysMach.pas
+++ PhysMach/PhysMach.pas
@@ -12,7 +12,7 @@ unit PhysMach;
 { imported units below.and is platform independant currently		}
 { Linux and windows on PC Hardware is supported				}
 
-{ $Id$S }
+{ $Id$ }
 
 interface
 
@@ -24,6 +24,11 @@ const
 	cnt_max			= 16;
 	tim_max			= 16;
 	analog_max		= 64;
+	DeviceTypeMax		= 16;
+
+
+type
+	DeviceTypeArray		= array[1..DeviceTypeMax] of char;
 
 var
 	marker 			: array[1..marker_max]   of boolean;
@@ -50,6 +55,7 @@ var
 	o_devicetype,
 	c_devicetype,
 	a_devicetype 		: array [1..group_max] of char;
+	DeviceList		: DeviceTypeArray;
 
 
 procedure PhysMachInit;
@@ -59,6 +65,8 @@ procedure PhysMachCounter;
 procedure PhysMachloadCfg(cfgFilename : string);
 procedure PhysMachReadAnalog;
 procedure PhysMachTimer;
+function  PhysMachGetDevices:DeviceTypeArray;
+procedure PhysMachIOByDevice(DeviceType:char);
 
 implementation
 
@@ -85,6 +93,163 @@ var
 	x			: word;
 
 
+
+//Private functions
+procedure PhysMachReadDevice(IOGroup:LongInt);
+var
+	wert,i           	: byte;
+	DeviceType		: Char;
+	Address			: LongInt;
+
+begin
+	DeviceType:=i_devicetype[IOGroup];
+	Address:=i_address[IOGroup];
+	if debug then writeln('PhysMachReadDevice IOGroup=',IOGroup,' DeviceType=',DeviceType,' Address=',Address);
+	case DeviceType of
+{$ifdef LINUX}
+		'D'	: wert:=dil_read_ports(Address);
+		'L'	: wert:=lp_read_ports(Address);
+		'P'	: wert:=pio_read_ports(Address);
+		'J'	: wert:=joy_read_ports(Address);
+{$endif}
+		'R'	: wert:=rnd_read_ports(Address);
+		'I'	: wert:=iow_read_ports(Address);
+		'H' 	: wert:=http_read_ports(Address);
+		'B' 	: wert:=bmcm_read_ports(Address);
+		'E'	: wert:=exec_read_ports(Address);
+	end;
+
+	if (debugFlag) then 
+		writeln	('PhysMach:PhysMachReadDigital   group ',IOGroup,' -> ',wert); 
+	for i:=7 downto 0 do begin	
+		if wert>=power[i] then begin
+			{ the adressing needs some explanation }
+			{ if IOGroup is 2 we calculate the base address (3-1)*8+1=17 the base of IOGroup 2 }
+			{ than we can add the loop counter to count down from 24 to 17 }
+	   		eingang[(IOGroup-1)*8+1+i]:=true;
+			wert:=wert-power[i]
+		end
+		else 
+			eingang[(IOGroup-1)*8+1+i]:=false;
+		if (debugFlag ) then begin
+			if ( i=7 ) then write('E group ',IOGroup,'   ');
+		   	write (eingang[(IOGroup-1)*8+1+i],' ');
+		   	if (i=0 ) then writeln;
+		end
+	end;
+	
+end;
+
+
+
+procedure PhysMachWriteDevice(IOGroup:LongInt);
+var
+	k,Value			: byte;
+	DeviceType		: Char;
+	Address			: LongInt;
+
+begin
+	DeviceType:=o_devicetype[IOGroup];
+	if (DeviceType <> '-') then begin
+		Address:=o_address[IOGroup];
+		Value:=0;
+		for  k:=7 downto 0 do begin
+			Value:=Value+power[k]*ord(ausgang[k+(IOGroup-1)*8+1]);
+			if (debugFlag ) then begin
+		 		if ( k=7 ) then write('A group ',IOGroup,'    ');
+		 		write (ausgang[k+(IOGroup-1)*8+1],' ');
+		 		if (k=0 ) then writeln;
+			end;		
+		end;
+
+
+		case DeviceType of
+{$ifdef LINUX}
+			'D'	: dil_write_ports(Address,Value);
+			'L'	: lp_write_ports(Address,Value);
+			'P'	: pio_write_ports(Address,Value);
+			'J'	: joy_write_ports(Address,Value);
+{$endif}
+			'R' 	: rnd_write_ports(Address,Value);
+			'I'	: iow_write_ports(Address,Value);
+			'H' 	: http_write_ports(Address,Value);
+			'B' 	: bmcm_write_ports(Address,Value);
+			'E'	: exec_write_ports(Address,Value);
+		end;
+	end;
+end;
+
+
+procedure PhysMachReadAnalogDevice(IOGroup:LongInt);
+begin
+	if (a_devicetype[IOGroup] <> '-') then
+		case a_devicetype[IOGroup] of
+{$ifdef LINUX}
+			'J' 	: analog_in[IOGroup]:=joy_read_ports(a_address[IOGroup]);
+			'B' 	: analog_in[IOGroup]:=bmcm_read_analog(a_address[IOGroup]);
+{$endif}
+			'H'	: analog_in[IOGroup]:=http_read_ports(a_address[IOGroup]);
+			'E'	: analog_in[IOGroup]:=exec_read_analog(a_address[IOGroup]);
+		end;
+	if (debugFlag) then writeln('Analog_in[',IOGroup,']=',analog_in[IOGroup]);
+end;
+
+
+
+procedure PhysMachReadCounter(IOGroup:LongInt);
+
+var
+	c,wert		: Byte;
+
+begin
+		if (c_devicetype[IOGroup] <> '-') then begin 
+
+			if debug then writeln('reading Counter type ',c_devicetype[IOGroup],' Adresse ',c_address[IOGroup]);
+
+			{ ZAEHLEReingaenge lesen  }
+			case c_devicetype[IOGroup] of
+{$ifdef LINUX}
+				'D'	: wert:=dil_read_ports(c_address[IOGroup]);
+				'L'	: wert:=lp_read_ports(c_address[IOGroup]);
+				'P'	: wert:=pio_read_ports(c_address[IOGroup]);
+				'J'	: wert:=joy_read_ports(c_address[IOGroup]);
+{$endif}
+				'R'	: wert:=rnd_read_ports(c_address[IOGroup]);
+				'I'	: wert:=iow_read_ports(c_address[IOGroup]);
+				'H' 	: wert:=http_read_ports(c_address[IOGroup]);
+				'B' 	: wert:=bmcm_read_ports(c_address[IOGroup]);
+				'E'	: wert:=exec_read_ports(c_address[IOGroup]);
+			end;
+		end
+		else
+			wert:=0;
+		
+		if debug then begin
+			writeln('Countervalue=',wert);
+			for c:=1 to 8 do write(' ',zust[c]);
+			writeln;
+		end;
+
+		for c:=1 to 8 do begin
+			{ zust[n] ist jeweils der vorherige Wert }
+			if wert mod 2 = 0 then zust[c+IOGroup-1]:=false 			{ wenn low dann 0 speichern	}
+			else	{ zust[] ist high }
+				if not(zust[c+IOGroup-1]) then begin 				{ wenn pos. Flanke am Eingang }
+					zust[c+IOGroup-1]:=true;		  		{ dann 1 speichern }
+					if z[c+IOGroup-1]>0 then dec(z[c+IOGroup-1]);	{ und ISTwert herunterz�en } 
+					if z[c+IOGroup-1]=0 then zahler[c+IOGroup-1]:=true;   	{ wenn ISTwert 0 dann ZAHLER 1}
+				end;
+			wert := wert div 2;
+		end;
+end;
+
+
+
+
+// exported functions
+
+
+
 procedure PhysMachloadCfg(cfgFilename : string);
 var
 	f				: text;
@@ -93,6 +258,8 @@ var
 	initstring			: string;
 	dir				: shortString;
 	iogroup				: integer;
+	i,NumOfDevices			: Byte;
+	AlreadyInList			: Boolean;
 	
 begin
 	assign (f,cfgFilename);
@@ -102,6 +269,7 @@ begin
 		writeln (' Config-File nicht gefunden');
 		halt(1);
 	end;
+	NumOfDevices:=1;
 	while not(eof(f)) do begin
 		readln (f,zeile);
 		if ( copy(zeile,1,6) = 'DEVICE' ) then begin
@@ -151,8 +319,21 @@ begin
 						exec_hwinit(initstring);
 						HWPlatform:=HWPlatform+',ext. APP  ';
 					  end;
-			end;	
-			
+			end;
+	
+			AlreadyInList:=false;
+			for i:=1 to NumOfDevices do
+				if (DeviceList[i]=initdevice) then AlreadyInList:=true;
+
+			if not(AlreadyInList) then begin
+				DeviceList[NumOfDevices]:=initdevice;
+				inc(NumOfDevices);
+			end;
+
+			if (NumOfDevices>DeviceTypeMax) then begin
+				writeln('Number of used Devicetypes exceeds limit!');
+				halt(1);
+			end;
 		end
 		else if (copy(zeile,1,4) = 'PORT') then begin
 			if ( debugFlag ) then writeln ('port detected');
@@ -215,95 +396,41 @@ begin
 		c_devicetype[x]:='-';
 		a_devicetype[x]:='-';
 	end;
+	for x:=1 to DeviceTypeMax do 
+		DeviceList[x]:='-';
+
 	hwPlatform:='';
 end;                               { ****ENDE INIT ****}
+
+
 
 
 procedure PhysMachReadDigital;               { liesst eingangswerte ein }
 
 var  
-	wert,i           	: byte;
-	io_group,x			: integer;
+	io_group		: integer;
 
 begin
 	{ for every configured input the port must be read }
-	x:=1;
+	io_group:=1;
 	repeat
-		io_group:=round(int(x/8)+1);
-		if (i_devicetype[io_group] <> '-') then 
-			case i_devicetype[io_group] of
-{$ifdef LINUX}
-				'D'	: wert:=dil_read_ports(i_address[io_group]);
-				'L'	: wert:=lp_read_ports(i_address[io_group]);
-				'P'	: wert:=pio_read_ports(i_address[io_group]);
-				'J'	: wert:=joy_read_ports(i_address[io_group]);
-{$endif}
-				'R'	: wert:=rnd_read_ports(i_address[io_group]);
-				'I'	: wert:=iow_read_ports(i_address[io_group]);
-				'H' 	: wert:=http_read_ports(i_address[io_group]);
-				'B' 	: wert:=bmcm_read_ports(i_address[io_group]);
-				'E'	: wert:=exec_read_ports(i_address[io_group]);
-			end	
-		else
-			wert:=0;
-		if (debugFlag) then 
-			writeln	('PhysMach:PhysMachReadDigital   group ',io_group,' -> ',wert); 
-		for i:=7 downto 0 do begin	
-			if wert>=power[i] then begin
-		   		eingang[x+i]:=true;
-				wert:=wert-power[i]
-			end
-			else 
-				eingang[x+i]:=false;
-			if (debugFlag ) then begin
-				if ( i=7 ) then write('E group ',x,'   ');
-			   	write (eingang[x+i],' ');
-			   	if (i=0 ) then writeln;
-			end 	  
-		end;
-		x:=x+8
-	until ( x > io_max );
+		PhysMachReadDevice(io_group);
+		inc(io_group);
+	until ( io_group > group_max );
 end;
-
 
 
 procedure PhysMachWriteDigital;              { gibt Ausg.werte an I/O Hardware aus}
 var 
-	k,wert				: byte;
-	io_group,x			: integer;
+	io_group			: integer;
 	
 begin
-	x:=1;
+	io_group:=1;
 	repeat
-		wert:=0;
-		for  k:=7 downto 0 do begin
-			wert:=wert+power[k]*ord(ausgang[k+x]);
-			if (debugFlag ) then begin
-		 		if ( k=7 ) then write('A group ',x,'    ');
-		 		write (ausgang[k+x],' ');
-		 		if (k=0 ) then writeln;
-			end;		
-		end;
-		io_group:=round(int(x/8)+1);
-		if (o_devicetype[io_group] <> '-') then 
-			if debug then writeln('writing device ',o_devicetype[io_group],' Addr ',o_address[io_group],' Value ',wert);
-			case o_devicetype[io_group] of
-{$ifdef LINUX}
-				'D'	: dil_write_ports(o_address[io_group],wert);
-				'L'	: lp_write_ports(o_address[io_group],wert);
-				'P'	: pio_write_ports(o_address[io_group],wert);
-				'J'	: joy_write_ports(o_address[io_group],wert);
-{$endif}
-				'R' 	: rnd_write_ports(o_address[io_group],wert);
-				'I'	: iow_write_ports(o_address[io_group],wert);
-				'H' 	: http_write_ports(o_address[io_group],wert);
-				'B' 	: bmcm_write_ports(o_address[io_group],wert);
-				'E'	: exec_write_ports(o_address[io_group],wert);
-			end;	
-		x:=x+8;
-	until ( x > io_max );
+		PhysMachWriteDevice(io_group);
+		inc(io_group);
+	until ( io_group > group_max );
 end;					{ **** ENDE SET_OUTPUT **** }
-
 
 
 procedure PhysMachCounter;		{ zaehlt timer und counter herunter liesst counter hardware }
@@ -313,55 +440,13 @@ var
 	x,io_group			: integer;
 
 begin
-	for c:=1 to tim_max do begin
-		if t[c] > 0 then t[c]:=t[c]-1;	{ Zeitzaehler decrementieren  }
-		if t[c]=0 then timer[c]:=true	{ zeitzaehler = 0? ja ==> TIMER auf 1}
-	end;
+	PhysMachTimer;
 	x:=1;
 	repeat
 		io_group:=round(int(x/8)+1);
-		if (c_devicetype[io_group] <> '-') then begin 
-
-			if debug then writeln('reading Counter type ',c_devicetype[io_group],' Adresse ',c_address[io_group]);
-
-			{ ZAEHLEReingaenge lesen  }
-			case c_devicetype[io_group] of
-{$ifdef LINUX}
-				'D'	: wert:=dil_read_ports(c_address[io_group]);
-				'L'	: wert:=lp_read_ports(c_address[io_group]);
-				'P'	: wert:=pio_read_ports(c_address[io_group]);
-				'J'	: wert:=joy_read_ports(c_address[io_group]);
-{$endif}
-				'R' 	: wert:=rnd_read_ports(c_address[io_group]);
-				'I'	: wert:=iow_read_ports(c_address[io_group]);
-				'H' 	: wert:=http_read_ports(c_address[io_group]);
-				'B' 	: wert:=bmcm_read_ports(c_address[io_group]);
-				'E'	: wert:=exec_read_ports(c_address[io_group]);
-			end
-		end
-		else
-			wert:=0;
-		
-		if debug then begin
-			writeln('Countervalue=',wert);
-			for c:=1 to 8 do write(' ',zust[c]);
-			writeln;
-		end;
-
-		for c:=1 to 8 do begin
-			{ zust[n] ist jeweils der vorherige Wert }
-			if wert mod 2 = 0 then zust[c+x-1]:=false 			{ wenn low dann 0 speichern	}
-			else	{ zust[] ist high }
-				if not(zust[c+x-1]) then begin 				{ wenn pos. Flanke am Eingang }
-					zust[c+x-1]:=true;		  		{ dann 1 speichern			}
-					if z[c+x-1]>0 then z[c+x-1]:=z[c+x-1]-1;	{ und ISTwert herunterz�en } 
-					if z[c+x-1]=0 then zahler[c+x-1]:=true;   	{ wenn ISTwert 0 dann ZAHLER 1}
-				end;
-			wert := wert div 2;
-		end;
+		PhysMachReadCounter(io_group);
 		x:=x+8;
 	until ( x > cnt_max );
-	//for c:=1 to cnt_max do  if z[c]=0 then zahler[c]:=true;
 end;                               { **** ENDE COUNT_DOWN ****       }
 
 
@@ -373,16 +458,7 @@ var
 begin
 	x:=0;
 	repeat
-		if (a_devicetype[x] <> '-') then
-			case a_devicetype[x] of
-{$ifdef LINUX}
-				'J' 	: analog_in[x+1]:=joy_read_ports(a_address[x],x);
-				'B' 	: analog_in[x]:=bmcm_read_analog(a_address[x]);
-{$endif}
-				'H'	: analog_in[x+1]:=http_read_ports(a_address[x]);
-				'E'	: analog_in[x]:=exec_read_analog(a_address[x]);
-			end;
-		if (debugFlag) then writeln('Analog_in[',x+1,']=',analog_in[x+1]);
+		PhysMachReadAnalogDevice(x);
 		inc(x);
 	until ( x > analog_max );
 end;
@@ -410,13 +486,51 @@ begin
 		end;
 		durchlauf100:=0;
 	end;
-	for c:=13 to 16 do begin
+	for c:=13 to 16 do begin				{ as fast as program runs }
 		if t[c] >= 0 then t[c]:=t[c]-1;
 		if t[c]=0 then timer[c]:=true
 	end;
 
 	{ END OF TIMER }
 end;
+
+
+
+function  PhysMachGetDevices:DeviceTypeArray;
+begin
+	PhysMachGetDevices:=DeviceList;
+end;
+
+
+
+procedure PhysMachIOByDevice(DeviceType : char);
+var
+	IOGroup			: Integer;
+	i,wert			: Byte;
+
+begin
+	IOGroup:=1;
+	repeat
+		// handle input type
+		if (i_devicetype[IOGroup] = DeviceType) then
+			PhysMachReadDevice(IOGroup);
+
+		// handle output type
+		if (o_devicetype[IOGroup] = DeviceType ) then
+			PhysMachWriteDevice(IOGroup);
+
+		// handle analog inputs
+		if (a_devicetype[IOGroup] = DeviceType) then
+			PhysMAchReadAnalogDevice(IOGroup);
+
+		// handle counters
+		if (c_devicetype[IOGroup] = DeviceType) then
+			PhysMachReadCounter(IOGroup);
+
+		inc(IOGroup);
+	until (IOGroup > group_max);
+end;
+
 
 
 begin
