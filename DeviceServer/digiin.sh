3ba9a1965080d9228719d6689590d743d77e155c
Hartmut Eilers <hartmut@eilers.net>
2008-03-20 23:44:45 +0000

    bash helper scripts to read digital and analog values and prepare them for exec_io_access api

---
+++ DeviceServer/digiin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this script is used as testprogram for reading digital values 
+# from external programs for my OpenLab Project
+
+# just do a echo "1 0 1 1 0 0 1 1">/tmp/digiin.dat
+#while true; do
+	cat /tmp/digiin.dat
+#done
+ 
