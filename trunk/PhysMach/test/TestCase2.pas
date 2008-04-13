cdb89bfc6b5481721b9e6f92011b8216433b7b7a
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2008-04-13 22:34:12 +0000

    + enhanced to make more stress

--- trunk/PhysMach/test/TestCase2.pas
+++ trunk/PhysMach/test/TestCase2.pas
@@ -9,14 +9,19 @@ uses CommonHelper;
 
 var
 	cmd	: String;
+	i	: LongInt;
 
 
 begin
 	writeln('TestCase2,  test RunCommand via CommonHelper');
 	writeln('(c) by Hartmut Eilers, 2008 - released under the GPL V2 or above');
 	writeln;
+
 	cmd:='digitemp.sh';
-	writeln(RunCommand(cmd));
+	for i:=1 to 1000 do begin
+		writeln('counter=',i,' Value=',RunCommand(cmd));
+	end;
+
 	writeln('bye');
 end.
 
