e3e70a94343bfdd319804f44b1c05486e222ef40
hucky <hucky@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2009-06-07 15:33:40 +0000

    + changed to allow easy debugging without hardware

--- trunk/DeviceServer/digitemp.sh
+++ trunk/DeviceServer/digitemp.sh
@@ -8,5 +8,6 @@
 # call digitemp , just take the last 2 sensors, just the temp in C, remove cr lf, remove the decimalpoint
 # resulting in a multiplication with 100
 # 35.26 degree celsius are returned as 3526
-result=`/usr/bin/digitemp_DS9097  -a| tail -n 2|cut -d " " -f 7|sed 's/^M/ /'|sed 's/\.//' `
+#result=`/usr/bin/digitemp_DS9097  -a| tail -n 2|cut -d " " -f 7|sed 's/^M/ /'|sed 's/\.//' `
+result=`cat /tmp/analog.dat`
 echo $result
