eda85f69bdb0e95f48145c1c956b9521216b2ac5
Hartmut Eilers <hartmut@eilers.net>
2010-03-07 15:37:41 +0000

    + first try to access Kolter IO Devices

--- PhysMach/kolterPCI_io_access.pas
+++ PhysMach/kolterPCI_io_access.pas
@@ -16,22 +16,51 @@ function kolterPCI_write_ports(io_port:longint;byte_value:byte):byte;
 function kolterPCI_hwinit(initdata:string;DeviceNumber:byte):boolean;
 
 implementation
+uses linux
+{$ifndef ZAURUS}
+,ports
+{$endif}
+;
 
+const	
+      debug             = false;
 
 function kolterPCI_read_ports(io_port:longint):byte;
+var	byte_value : byte;
 
 begin
+	{$ifndef ZAURUS}
+	ReadPort(io_port,byte_value);
+	if (debug) then writeln('Kolter_IO r : ',byte_value);
+	kolterPCI_read_ports:=byte_value;
+	{$endif}
 end;
 
 
 function kolterPCI_write_ports(io_port:longint;byte_value:byte):byte;	
 
 begin
+    if (debug) then  writeln ('Kolter_IO w : ',byte_value);
+	{$ifndef ZAURUS}
+	WritePort(io_port,byte_value);
+	{$endif}
+	kolterPCI_write_ports:=0;
 end;
 
 
 function kolterPCI_hwinit(initdata:string;DeviceNumber:byte):boolean;
+var
+        controlPort : LongInt;
+
 begin
+	val(initdata,controlPort);
+	if ( debug ) then
+		writeln('Kolter IO Device Port : ',controlPort);
+	{$ifndef ZAURUS}
+	{ allow full port access for the addressrange controlPort to controlPort + $FF }
+	IOperm(controlPort,$FF,$ff);
+	{$endif}
+	kolterPCI_hwinit:=true;
 end;
 
 
