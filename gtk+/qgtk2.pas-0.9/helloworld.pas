92ec7adef4af50732bb4e680c6a32e9abc86797d
Hartmut Eilers <hartmut@eilers.net>
2011-02-16 10:21:17 +0000

    obsoleted

--- gtk+/qgtk2.pas-0.9/helloworld.pas
+++
@@ -1,7 +0,0 @@
-uses qgtk2;                                                                      
-begin                                                                           
-qstart('Hello Word!', nil, nil);                                                
-qLabel('  Hello word with ');                                                   
-qButton(' QUIT button ', @qDestroy);                                            
-qGo;                                                                            
-end.
