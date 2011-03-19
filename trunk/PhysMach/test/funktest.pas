cf5d3834b6d4fa01e5eca8a4f500a54a9111a934
hartmut <hartmut@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2011-03-19 12:27:14 +0000

    + moved testprograms from other places to here

--- trunk/PhysMach/test/funktest.pas
+++ trunk/PhysMach/test/funktest.pas
@@ -7,12 +7,51 @@ uses funk_io_access;
 
 const IOPort=$3bc;
 
-var bits : byte;
+var 	bits,
+	a,b,c,d	: byte;
+	Knopf	: char;
 
 begin
-	funk_hwinit('1000');
+	funk_hwinit('1000',1);
+	a:=0;b:=0;c:=0;d:=0;
 	repeat
+{
+		readln(Knopf);
+		case Knopf of
+			'a' : 	if (a=0 ) then begin
+					a:=1;
+					funk_write_ports(IOPort,$ee);
+				end else begin
+					a:=0;
+					funk_write_ports(IOPort,$de);
+				end;
+			'b' : 	if (b=0 ) then begin
+					b:=1;
+					funk_write_ports(IOPort,$ed);
+				end else begin
+					b:=0;
+					funk_write_ports(IOPort,$dd);
+				end;
+			'c' : 	if (c=0 ) then begin
+					c:=1;
+					funk_write_ports(IOPort,$eb);
+				end else begin
+					c:=0;
+					funk_write_ports(IOPort,$db);
+				end;
+			'd' : 	if (d=0 ) then begin
+					d:=1;
+					funk_write_ports(IOPort,$e7);
+				end else begin
+					d:=0;
+					funk_write_ports(IOPort,$d7);
+				end;
+		end;
+
+	until Knopf='e';
+}
 		readln(bits);
 		funk_write_ports(IOPort,bits);
+
 	until bits=0;
 end.
