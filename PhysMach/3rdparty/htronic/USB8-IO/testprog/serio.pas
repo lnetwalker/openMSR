797fd2eee3b93ce504f7a57ee0ea093f27e1c9b7
Hartmut Eilers <hartmut@eilers.net>
2012-11-05 17:57:26 +0000

    Documentation about the HTronic USB8 IO device testprogram in pascal for linux

---
+++ PhysMach/3rdparty/htronic/USB8-IO/testprog/serio.pas
@@ -0,0 +1,45 @@
+program serio;
+
+{
+  Testprogram to read Analog Values from the board
+  compiled from different examples from different people
+  thanks to everyone making open source software
+}
+
+uses 
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes,SysUtils,Synaser,Crt
+  { you can add units after this };
+
+  procedure RS232_connect;
+  var
+     ser: TBlockSerial;
+     hi,lo,chk : byte;
+     value: word;
+  begin
+	ser:=TBlockSerial.Create;
+	ser.Connect('/dev/ttyACM0'); //ComPort
+	Sleep(1000);
+	ser.config(115000, 8, 'N', SB1, False, False);
+	Write('Device: ' + ser.Device + '   Status: ' + ser.LastErrorDesc +' '+
+	Inttostr(ser.LastError));
+	Sleep(1000);
+	repeat
+		ser.SendString('c01');
+                hi:=ser.RecvByte(10000);
+		lo:=ser.RecvByte(10000);
+		chk:=ser.RecvByte(10000);
+		if ( lo + hi <> chk ) then writeln ( 'Checksum Error');
+		value:= hi * 256 + lo;
+		writeln ( IntToHex(hi,2), ' ' ,IntToHex(lo,2), ' ' , IntToHex(chk,2) ,' => ', value);
+	until keypressed; //Important!!!
+	Writeln('Serial Port will be freed...');
+	ser.free;
+	Writeln('Serial Port was freed successfully!');
+  end;
+ 
+  begin
+     RS232_connect()
+  end.
