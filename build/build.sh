832e69f669269e3a6fd87bc70e7a95d615fd9035
Hartmut Eilers <hartmut@eilers.net>
2016-08-17 15:37:59 +0000

    add build error checking for errors while generating tar  archive

--- build/build.sh
+++ build/build.sh
@@ -82,7 +82,11 @@ find $BUILD_DIR -name ".svn" -exec rm -rf {} \;
 
 mkdir $RELEASE
 cp -a $BUILD_DIR/* $RELEASE/
-tar -czvf /data/src/Releases/$RELEASE.tar.gz $RELEASE
+tar --exclude=.svn -czvf /data/src/Releases/$RELEASE.tar.gz $RELEASE
+  if [ $? != 0 ]; then
+    echo "error: in building package $RELEASE.tar.gz"
+    exit 1
+  fi
 
 rm -rf $BUILD_DIR
 
