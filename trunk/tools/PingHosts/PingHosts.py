6c7fd3b2fd0dea7fb040e38db8ab32aae03f9bc5
lnetwalker <lnetwalker@a7a8561a-43b9-4a0f-9c6a-b419c09c6325>
2017-01-31 00:14:03 +0000

    tools to bring hostdata into DeviceServer

---
+++ trunk/tools/PingHosts/PingHosts.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+
+# OpenMSR Utility to ping hosts and set signals
+# in the OpenMSR DeviceServer depending wether
+# a host is reachable or not
+
+# import http library
+import requests
+
+# import useful stuff
+import sys, getopt, time, subprocess, ConfigParser
+from array import array
+import os, platform
+
+
+def main(argv):
+
+	def strip_html( raw ):
+		# strip off html stuff of OpenMSR IOgroup read answer
+		# and returns a String with the pure ones and zeros of
+		# the corresponding IOGroup
+		bare=raw.replace("<html><body>","")
+		bare=bare.replace("</body></html>","")
+		bare=bare.replace(" ","")
+		return bare
+
+	def ping(host):
+		# Returns True if host responds to a ping request
+		# Ping parameters as function of OS
+		ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
+		# Ping
+		return os.system("ping " + ping_str + " " + host) == 0
+
+	URL = ''
+	iogroup = ''
+	Found_url = False
+	Found_group = False
+	host = {}
+	pin = {}
+	PingState = {}
+
+  	# get the commandline parameters
+	try:
+		opts, args = getopt.getopt(argv, "hu:g:f:",["url=","group=", "file="])
+	except getopt.GetoptError:
+		print ('PingHosts.py -u <URL> -g <iogroup>  -f <configfile>')
+		sys.exit(2)
+	for opt, arg in opts:
+ 		if opt == '-h':
+			print ('PlaySound.py -u <URL> -g <iogroup> -f <configfile>')
+			sys.exit()
+		elif opt in ("-u", "--url"):
+			URL = arg
+			Found_url = True
+		elif opt in ("-g", "--group"):
+			iogroup = arg
+			Found_group = True
+		elif opt in ("-f", "--file"):
+			conf_file = arg
+			Found_file = True
+
+  	# read the config file
+	Config = ConfigParser.ConfigParser()
+	Config.read(conf_file)
+
+  	# get sections of Config
+	ConfigSections = Config.sections()
+
+	# get general settings of DeviceServer
+	if not Found_url:
+		URL =  Config.get ( 'DeviceServer', 'URL' )
+		if URL != '':
+			Found_url = True
+	if not Found_group:
+		iogroup = Config.get ( 'DeviceServer', 'IOgroup' )
+		if iogroup != '':
+			Found_group = True
+
+  	# the url, the iogroup and file are mandatory so if one is missing stop
+	if ( not Found_url or not Found_group or not Found_file):
+		print ('PingHosts.py -u <URL> -g <iogroup> -f <configfile>')
+		print ('Missing parameter, -f is mandantory, rest can be configured in file')
+		sys.exit(2)
+
+  	# build conf variables which hold all needed Data
+	confCounter = 1
+	for ConfigSection in ConfigSections:
+		if ConfigSection != 'DeviceServer':
+			host[confCounter] = Config.get ( ConfigSection, 'Host' )
+			pin[confCounter] = Config.get ( ConfigSection, 'Pin' )
+			print ("Conf Counter : " , confCounter , " Found Host: " + host[confCounter] + " Pin: " + pin[confCounter]  + " in Section: " + ConfigSection)
+			confCounter += 1
+
+  	# run the loop
+	while (True):
+
+		Error = False
+  		# loop over the configured hosts
+		for current in range(1,len(host)+1):
+  			# save Hoststate true if host is reachable
+			pingresult = ping (host[current])
+			if pingresult:
+				PingState[current-1] = "1" # PingState[current-1] minus one because PingState Array starts with 0 !
+			else:
+				 PingState[current-1] = "0"
+			print URL + '?' + iogroup + ',' + pin[current] + ',' + PingState[current-1]
+			try:
+				Response = requests.get(URL + '?' + iogroup + ',' + pin[current] + ',' + PingState[current-1])
+			except:
+				Error = True
+
+			if Error:
+				print "Error write URL " + URL + '?' + iogroup
+  		#/for current ...
+
+
+		# write the results to DeviceServer
+
+	#/while
+#/main
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ trunk/tools/hostdata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this script is used to gather data of the host running the Deviceserver
+# as an example the load and the amount of free space on / is shown
+# can be used by PhysMach
+# (c) 2016 by Hartmut Eilers
+# licensed under the GNU GPL V2
+
+# get the 1 minute load and multiply by 100
+load_1min=`top -n1|head -n1|cut -d " " -f15|sed 's/\.//'|sed 's/,//'`;
+
+# alternative you can use the following cmd to get the int(1 minute load)
+# top -n1|head -n1|cut -d " " -f15|cut -d "." -f1
+
+diskfree=`df -kh |egrep "/$" |head -n1|tr -s " "|cut -d " " -f5|sed 's/%//ig'`;
+
+echo $load_1min $diskfree|sed 's/,//'
